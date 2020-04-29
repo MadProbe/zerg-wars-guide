8aec0777a7cd1ebae837fa1cf87040bdb32c6242
MadProbe <49519179+MadProbe@users.noreply.github.com>
2020-04-29 18:45:34 +0300

    rewrite server to pure https.

--- dev/_server/_simple_server.js
+++ dev/_server/_simple_server.js
@@ -1,24 +1,67 @@
-import express from "express";
-import { createServer } from "https";
-import { readFileSync, readdirSync } from "fs";
-import { join } from "path";
-import { compressed } from "./_compressed.js";
-import { files } from "./_static_router.js";
-const args = process.argv.filter(arg => !process.execArgv.includes(arg));
-const dir = process.cwd();
-const server = express();
-const port = args.find(arg => /--port=\d\d\d\d/gi.test(arg));
-server.use('/cdn/css', files(join(dir, 'cdn/css')));
-server.use('/cdn/img', files(join(dir, 'cdn/img')));
-server.use('/cdn/js', files(join(dir, 'cdn/js')));
-server.use('/cdn/md', files(join(dir, 'cdn/md')));
-readdirSync(dir, { 'withFileTypes': true }).forEach(file => {
-    if (file.isFile() && file.name.endsWith('.html')) {
-        const name = join(dir, file.name)
-        server.get('/' + file.name, compressed(name, 'text/html'))
-    }
-});
-createServer({
-    cert: readFileSync('./dev/_server/cert/cert.pem'),
-    key: readFileSync('./dev/_server/cert/cert.key')
-}, server).listen(port ? port.match(/\d\d\d\d/)[0] : 8080, () => console.log('Ready!'));
+// @ts-check
+const https = require("https");
+const mime_types = require("mime-types");
+const zlib = require("zlib");
+const { readFileSync, createReadStream, access, constants } = require("fs");
+const { join } = require("path");
+const { URL } = require("url");
+const { port, dirs } = require("./_config.js");
+const { pipeline } = require("stream");
+const cwd = process.cwd();
+const _404Location = join(cwd, 'dev', '_server', '_404.html');
+const _500Location = join(cwd, 'dev', '_server', '_500.html');
+https.createServer({
+	cert: readFileSync('./dev/_server/cert/cert.pem'),
+	key: readFileSync('./dev/_server/cert/cert.key')
+}, (request, response) => {
+	const url = new URL(request.url, `https://${request.headers.host}`);
+	const path = dirs.map(regex => regex.exec(url.pathname)).find(path => path != null);
+	if (path) {
+		const file = join(cwd, path[0]);
+		access(file, constants.R_OK, e => {
+			if (e) {
+				if (e.code !== 'ENOENT') {
+					console.error('\x1b[31m[ERROR]\x1b[0m', `Can't access "${file}" file!`);
+					response.writeHead(500, { 'Content-Type': 'text/html' });
+					createReadStream(_500Location).pipe(response);
+				} else {
+					response.writeHead(404, { 'Content-Type': 'text/html' });
+					createReadStream(_404Location).pipe(response);
+				}
+				return;
+			}
+			response.setHeader('Content-Type', mime_types.lookup(file) || 'text/plain');
+			response.setHeader('Vary', 'Accept-Encoding');
+			response.setHeader('Cache-Control', 'max-age=31536000');
+			const acceptEncoding = request.headers['accept-encoding'].toString();
+			const raw = createReadStream(file);
+			const onError = err => {
+				if (err) {
+					if (response.writableLength !== 0 || response.writableEnded) {
+						response.end();
+						console.error('An error occurred:', err);
+					} else {
+						console.error('\x1b[31m[ERROR]\x1b[0m', `Can't read "${file}" file!`);
+						createReadStream(_500Location).pipe(response);
+					}
+				}
+			};
+			if (/\bdeflate\b/.test(acceptEncoding)) {
+				response.writeHead(200, { 'Content-Encoding': 'deflate' });
+				pipeline(raw, zlib.createDeflate(), response, onError);
+			} else if (/\bgzip\b/.test(acceptEncoding)) {
+				response.writeHead(200, { 'Content-Encoding': 'gzip' });
+				pipeline(raw, zlib.createGzip(), response, onError);
+			} else if (/\bbr\b/.test(acceptEncoding)) {
+				response.writeHead(200, { 'Content-Encoding': 'br' });
+				pipeline(raw, zlib.createBrotliCompress(), response, onError);
+			} else {
+				response.writeHead(200);
+				pipeline(raw, response, onError);
+			}
+		});
+	} else {
+		response.writeHead(404, { 'Content-Type': 'text/html' });
+		createReadStream(_404Location).pipe(response);
+	}
+}).listen(port || 8080, () => console.log(`Server has started on https://localhost:${port || 8080}`));
