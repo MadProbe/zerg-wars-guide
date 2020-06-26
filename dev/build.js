56d139ebf03a8093987f41e439e79c4a8a59a50b
unknown <49519179+MadProbe@users.noreply.github.com>
2020-06-26 21:44:00 +0300

    faster building & read file caching & <include> in included files

--- dev/build.js
+++ dev/build.js
@@ -5,6 +5,7 @@ const { minify } = require("csso");
 const { minify: htmlMinify } = require("html-minifier");
 const LooseMarkdown = require('./md.js');
 
+const $cache = new Map;
 const cwd = join(__dirname, '..');
 const toBuildDir = join(__dirname, '_to_build');
 const cssMinifyOptions = {
@@ -41,29 +42,45 @@ exec('npm run css-build', {
         writeFileSync(join(cwd, 'cdn', 'css', 'merged.min.css'), minifiedCSSCode);
     }
 });
+function transformHMTL(code) {
+    return code.replace(/<include\s+src\s*=\s*"(.+)"(\s+(?:\w+(?:\s*=\s*"([^>"]+)")?))*>\s*<\/include>/g, (_, path, attrs) => {
+        path = join(cwd, path);
+        if (!$cache.has(path)) {
+            $cache.set(path, readFileSync(path, 'utf8'));
+        }
+        const read = $cache.get(path);
+        switch (extname(path).slice(1)) {
+            case "md":
+                return LooseMarkdown(read);
+            
+            case "html":
+                return transformHMTL(read);
+        
+            default:
+                return read;
+        }
+    })
+}
 function buildHTML(file, ...dists) {
-    let code = readFileSync(file, 'utf8');
-    code = code.replace(/<include\s+src\s*=\s*"(.+)">\s*<\/include>/g, (_, path) => extname(path) === '.md' ?
-        LooseMarkdown(readFileSync(path, 'utf8')) :
-        readFileSync(path, 'utf8'));
+    const code = transformHMTL(readFileSync(file, 'utf8'));
+    const minified = htmlMinify(code, {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: true,
+        decodeEntities: true,
+        minifyCSS: true,
+        minifyJS: { ie8: true },
+        minifyURLs: true,
+        sortAttributes: true,
+        sortClassName: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        removeRedundantAttributes: true,
+        removeTagWhitespace: true
+    });
     for (let index = 0, length = dists.length; index < length; index++) {
-        writeFileSync(dists[index], htmlMinify(code, {
-            collapseWhitespace: true,
-            collapseBooleanAttributes: true,
-            removeAttributeQuotes: true,
-            decodeEntities: true,
-            collapseWhitespace: true,
-            minifyCSS: true,
-            minifyJS: { ie8: true },
-            minifyURLs: true,
-            sortAttributes: true,
-            sortClassName: true,
-            removeScriptTypeAttributes: true,
-            removeStyleLinkTypeAttributes: true,
-            removeComments: true,
-            removeEmptyAttributes: true,
-            removeRedundantAttributes: true,
-            removeTagWhitespace: true
-        }), { encoding: 'utf8' });
+        writeFileSync(dists[index], minified, { encoding: 'utf8' });
     }
 }
