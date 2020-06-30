addf7374bfbc4a540d87bb57e746508adc012300
unknown <49519179+MadProbe@users.noreply.github.com>
2020-06-30 17:42:04 +0300

    apply dark theme immediately

--- cdn/js/dark-theme.js
+++ cdn/js/dark-theme.js
@@ -1,20 +1,19 @@
 (function (window, document, localStorage, key) {
-    var classList = document.documentElement.classList;
-    var button = document.querySelector('.theme-toggle');
-    var list = document.querySelector('#theme-toggle').classList;
-    var text = document.querySelector('#button-text');
     var state = localStorage.getItem(key) !== "false";
-    window.onstorage = function (event) {
-        if (event.key === key) {
-            setTheme(event.newValue === "true");
-        }
+    var querySelector = document.querySelector.bind(document);
+    var classList = querySelector('html').classList;
+    var button;
+    var list;
+    var text;
+    if (state) {
+        classList.add('dark');
     }
-    function setTheme(dark) {
+    function setTheme(dark, add) {
         if (dark) {
             text.innerHTML = "Dark Theme Enabled"
             list.add('fa-moon-o');
             list.remove('fa-sun-o');
-            classList.add('dark');
+            add || classList.add('dark');
         } else {
             text.innerHTML = "Light Theme Enabled"
             list.add('fa-sun-o');
@@ -23,8 +22,19 @@
         }
         localStorage.setItem(key, dark);
     }
-    setTheme(state);
-    button.onclick = function () {
-        setTheme(state = !state);
+    window.onload = function () {
+        button = querySelector('.theme-toggle');
+        list = querySelector('#theme-toggle').classList;
+        text = querySelector('#button-text');
+        setTheme(state, true);
+        window.onstorage = function (event) {
+            if (event.key === key) {
+                setTheme(event.newValue === "true");
+            }
+        }
+        button.onclick = function () {
+            setTheme(state = !state);
+            localStorage.setItem(key, dark);
+        }
     }
-})(window, document, localStorage, "dark-theme");
+})(this, document, localStorage, "dark-theme")
