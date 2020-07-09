0fa21a8d25456de37e9c744bd0bb56c7da30f0d1
unknown <49519179+MadProbe@users.noreply.github.com>
2020-07-10 00:00:01 +0300

    no errors on theme toggle

--- cdn/js/dark-theme.js
+++ cdn/js/dark-theme.js
@@ -1,40 +1,38 @@
-(function (window, document, localStorage, key) {
+// @ts-check
+((window, document, localStorage, key, _theme_enabled) => {
     var state = localStorage.getItem(key) !== "false";
     var querySelector = document.querySelector.bind(document);
     var classList = querySelector('html').classList;
+    var setTheme = (dark, add) => {
+        if (dark) {
+            text.innerHTML = "Dark" + _theme_enabled;
+            list.add('moon');
+            list.remove('sun');
+            add || classList.add('dark');
+        } else {
+            text.innerHTML = "Light" + _theme_enabled;
+            list.add('sun');
+            list.remove('moon');
+            classList.remove('dark');
+        }
+        localStorage.setItem(key, dark);
+    }
     var button;
     var list;
     var text;
     if (state) {
         classList.add('dark');
     }
-    function setTheme(dark, add) {
-        if (dark) {
-            text.innerHTML = "Dark Theme Enabled"
-            list.add('fa-moon-o');
-            list.remove('fa-sun-o');
-            add || classList.add('dark');
-        } else {
-            text.innerHTML = "Light Theme Enabled"
-            list.add('fa-sun-o');
-            list.remove('fa-moon-o');
-            classList.remove('dark');
-        }
-        localStorage.setItem(key, dark);
-    }
-    window.onload = function () {
-        button = querySelector('.theme-toggle');
-        list = querySelector('#theme-toggle').classList;
-        text = querySelector('#button-text');
+    onload = _ => {
+        button = querySelector('.tt');
+        list = querySelector('#tt').classList;
+        text = querySelector('#bt');
         setTheme(state, true);
-        window.onstorage = function (event) {
+        onstorage = (event) => {
             if (event.key === key) {
                 setTheme(event.newValue === "true");
             }
         }
-        button.onclick = function () {
-            setTheme(state = !state);
-            localStorage.setItem(key, dark);
-        }
+        button.onclick = _ => setTheme(state = !state);
     }
-})(this, document, localStorage, "dark-theme")
+})(this, document, localStorage, "dark-theme", " Theme Enabled")
