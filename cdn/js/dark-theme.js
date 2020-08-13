c341efa8bcb8c5499ac218d7d802bb7c5f0ff2e6
unknown <49519179+MadProbe@users.noreply.github.com>
2020-08-13 23:18:51 +0300

    => to function

--- cdn/js/dark-theme.js
+++ cdn/js/dark-theme.js
@@ -1,8 +1,8 @@
-((document, localStorage, key, _theme_enabled) => {
+(function (document, localStorage, key, _theme_enabled) {
     var state = localStorage.getItem(key) !== "false";
     var querySelector = document.querySelector.bind(document);
     var classList = querySelector('html').classList;
-    var setTheme = (dark, add) => {
+    var setTheme = function (dark, add) {
         if (dark) {
             text.innerHTML = "Dark" + _theme_enabled;
             list.add('moon');
@@ -22,16 +22,18 @@
     if (state) {
         classList.add('dark');
     }
-    onload = _ => {
+    onload = function () {
         button = querySelector('.tt');
         list = querySelector('#tt').classList;
         text = querySelector('#bt');
         setTheme(state, true);
-        onstorage = (event) => {
-            if (event.key === key) {
+        onstorage = function (event) {
+            if (event.key === key && event.target !== window) {
                 setTheme(event.newValue === "true");
             }
         }
-        button.onclick = _ => setTheme(state = !state);
+        button.onclick = function () {
+            setTheme(state = !state);
+        }
     }
 })(document, localStorage, "dark-theme", " Theme Enabled")
